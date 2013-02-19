4c84e96d57d45db7f960b030eddc77648ae2fa5a
Jorge <jorge@jorge-tecra.(none)>
2013-02-19 15:19:33 +0000

    Implement print_help() like in GNU Hello

--- args.c
+++ args.c
@@ -4,12 +4,30 @@
 
 void print_help(void)
 {
-	puts("HELP\n");
+  //TODO: this should not be staticaly defined
+  const char* program_name = "hello-world";
+  const char* package_bugreport = "bug-report";
+  const char* package_name = "package_name";
+  const char* package_url = "package_url";
+
+        printf ("\
+Usage: %s [OPTION]...\n", program_name);
+
+  printf("\
+Print a friendly, customizable greeting.\n", stdout);
+  printf("\
+  -h, --help          display this help and exit\n\
+  -v, --version       display version information and exit\n", stdout);
+
+  printf("\n");
+  printf("\
+Report bugs to: %s\n", package_bugreport);
+  printf("%s home page: <%s>", package_name, package_url);
 }
 
 void print_version(void)
 {
-	puts("VERSION\n");
+	puts("VERSION");
 }
 
 void arguments(int argc, char **argv)
